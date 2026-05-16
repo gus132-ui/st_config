421dbe16579fbfabb91b52436e9234c72e900fb1
Lukasz Kasprzak <lukas@labunix.xyz>
2026-05-16 11:45:32 +0200

    added patch colors-at-launch

--- x.c
+++ x.c
@@ -2064,6 +2064,8 @@ usage(void)
 int
 main(int argc, char *argv[])
 {
+    int i;
+    char *colval;
 	xw.l = xw.t = 0;
 	xw.isfixed = False;
 	xsetcursor(cursorstyle);
@@ -2107,6 +2109,11 @@ main(int argc, char *argv[])
 		break;
 	case 'v':
 		die("%s " VERSION "\n", argv0);
+		break;
+    case 'C':
+        colval = strtok(EARGF(usage()), "@");
+        i = atoi(strtok(NULL, "@"));
+		colorname[i] = colval;
 		break;
 	default:
 		usage();
